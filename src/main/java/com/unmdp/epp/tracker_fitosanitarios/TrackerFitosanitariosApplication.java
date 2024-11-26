5781e5a34417a24bf4593d0b12067613c6e0b699
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2024-11-26 20:28:00 -0300

    init spring

---
+++ src/main/java/com/unmdp/epp/tracker_fitosanitarios/TrackerFitosanitariosApplication.java
@@ -0,0 +1,13 @@
+package com.unmdp.epp.tracker_fitosanitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerFitosanitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerFitosanitariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unmdp/epp/tracker_fitosanitarios/TrackerFitosanitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unmdp.epp.tracker_fitosanitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerFitosanitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
